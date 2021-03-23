fff211dc1a523e56e5de95f2abcdef1382e92c00
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-03-23 19:04:06 -0400

    added secions to html and questions/answers object in js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
-console.log("Hello world!");
+// DOM variables
+var startButton = document.querySelector("#start-button");
+
+var questions = [{
+        questions: "Commonly used data types DO NOT include:"
+        answers: ["string", "boolean", "alerts", "numbers"]
+        solution: "alerts"
+    },
+    {
+        questions: "The conditions in an if/else statement is enclosed within___."
+        answers: ["quotes", "curly brackets", "parentheses", "square brackets"]
+        solution: "curly brackets"
+    },
+    {
+        questions: "Arrays in JavaScript can be used to store ___."
+        answers: ["Numbers and strings", "other arrays", "booleans", "all of the above"]
+        solution: "all of the above"
+    },
+    {
+        questions: "String values must be enclosed within ___ when being assigned to variables."
+        answers: ["commas", "curly brackets", "quotes", "parentheses"]
+        solution: "parentheses"
+    },
+    {
+        questions: "A very useful tool used during development and debugging for printing content to the debugger is:"
+        answers: ["JavaScript", "terminal/bash", "for loops", "console log"]
+        solution: "console log"
+    },
+
+];
+
+// JavaScript variables
+
+
+// Function definitions
+
+
+
+// Event listeners
+startButton.addEventListener("click", function () {
+
+});
+// Function calls
