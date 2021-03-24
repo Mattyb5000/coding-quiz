225f6809ee71a4b46b3c5622bee6088644fdfa4e
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-03-24 00:06:53 -0400

    changed i < questions[0].answers.length to i < 4 in script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,65 @@
 // DOM variables
-var startButton = document.querySelector("#start-button");
-var question1 = document.querySelector("#question")
+var startButton = document.getElementById("start-button");
+var question1 = document.getElementById("question");
+var scoreEl = document.getElementById("score");
 
 var questions = [
-{
+  {
     question: "Commonly used data types DO NOT include:",
     answers: ["string", "boolean", "alerts", "numbers"],
-    solution: "alerts"
-},
-{
+    solution: "alerts",
+  },
+  {
     question: "The conditions in an if/else statement is enclosed within___.",
     answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    solution: "curly brackets"
-},
-{
+    solution: "curly brackets",
+  },
+  {
     question: "Arrays in JavaScript can be used to store ___.",
-    answers: ["Numbers and strings", "other arrays", "booleans", "all of the above"],
-    solution: "all of the above"
-},
-{
-    question: "String values must be enclosed within ___ when being assigned to variables.",
+    answers: [
+      "Numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    solution: "all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ___ when being assigned to variables.",
     answers: ["commas", "curly brackets", "quotes", "parentheses"],
-    solution: "parentheses"
-},
-{
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    solution: "parentheses",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: ["JavaScript", "terminal/bash", "for loops", "console log"],
-    solution: "console log"
-}
+    solution: "console log",
+  },
 ];
-// for (var i = 0; i < 5; i++) {
-// //     // This is the block of code that will run each time
-//     console.log("This is the current value of i: " + i + ".");
-// }
-//
-
-
 
 // Function definitions
-function startQuiz(){
-   question1.textContent = questions[1].question
-   for (var i = 0; i < questions[0].answers.length; i++){
-    // console.log(questions[0].answers[i])
-    document.getElementById('options').innerHTML += "<button>" + questions[1].answers[i] + "</button> <br/>"
-}
-}
+function startQuiz() {
+  question1.textContent = questions[0].question;
+  for (var i = 0; i < 4; i++) {
+    console.log(questions[0].answers[i])
+    document.getElementById("options").innerHTML +=
+      "<button>" + questions[0].answers[i] + "</button> <br/>";
 
+  }
+}
+questions[0].answers.length
+// function generateQuestion() {
+//   question1.textContent = questions[1].question;
+//   for (var i = 0; i < questions[1].answers.length; i++) {
+//     // console.log(questions[0].answers[i])
+//     document.getElementById("options").innerHTML +=
+//       "<button>" + questions[1].answers[i] + "</button> <br/>";
+//   }
+// }
 
 // Event listeners
 startButton.addEventListener("click", function () {
-    startQuiz()
+  startQuiz();
 });
 // Function calls
